e9d1482bada09e4b295564bd7b483ead2a378491
Islam <islamsakrak2002@gmail.com>
2022-02-15 15:48:52 +0100

    Replacing magic numbers with constants

--- src/main/java/VideoStoreDates.java
+++ src/main/java/VideoStoreDates.java
@@ -1,9 +1,10 @@
 import java.time.LocalDate;
 
 public class VideoStoreDates {
-
-    private final LocalDate due = LocalDate.now().plusWeeks(1);
-    private final LocalDate overDue = LocalDate.now().plusWeeks((int)(Math.random()*3));
+    private final int DUE_DATE = 1;
+    private final int OVERDUE_DATE = 3;
+    private final LocalDate due = LocalDate.now().plusWeeks(DUE_DATE);
+    private final LocalDate overDue = LocalDate.now().plusWeeks((int)(Math.random()*OVERDUE_DATE));
 
     public LocalDate getDue() {
         return due;
