14bc9452501ebe8e85fe102ad9887492ff037103
FoolProof <islamsakrak2002@gmail.com>
2022-08-28 16:29:55 +0200

    Renaming the start method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
         MovieStore movieStore = new MovieStore();
-        movieStore.start();
+        movieStore.startApplication();
     }
 }
